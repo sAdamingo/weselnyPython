b11b24af2d700103c23ee42e2785b979a552c0b2
Adam <ad.stelmach@gmail.com>
2020-11-16 12:03:20 +0100

    Applied suggested changes.

--- src/Task17.py
+++ src/Task17.py
@@ -1,3 +1,6 @@
+import re
+
+
 def read_content_from_file(filename):
     stream = open(filename, 'r', encoding='utf-8')
     try:
@@ -7,35 +10,30 @@ def read_content_from_file(filename):
         stream.close()
     return content
 
-def getSentencesOrWords(text, sentencesOrWords):
-    if sentencesOrWords:
-        sentences = text.split(sep=".")
-        return sentences
-    else:
-        words = text.lower().replace("?", "").replace(":", "").replace(";", "").replace("-", "").replace("!",
-                                                                                                         "").replace(
-            ".", "").replace(",", "").split(sep=" ")
-        return words
 
-def number_of_occurrences_of_each_word(words):
+def get_longest_sentence(text):
+    sentences = text.split(sep=".")
+    print("\nYour longest sentence is:\n", max(sentences, key=len))
+
+
+def get_number_of_occurrences_of_each_word(text):
+    words = re.findall(r'\w+', text.lower())
     word_counter = {}
     for word in words:
-         if word in word_counter:
-             occurrences = word_counter[word]
-             word_counter[word] = occurrences + 1
-         else:
-             word_counter[word] = 1
+        if word in word_counter:
+            occurrences = word_counter[word]
+            word_counter[word] = occurrences + 1
+        else:
+            word_counter[word] = 1
     sorted_dict = dict(sorted(word_counter.items(),
                               key=lambda item: item[1],
                               reverse=True))
-    return sorted_dict
+    print("Let me see how many occurrences of each word we have in your text: ")
+    for key, value in sorted_dict.items():
+        print(key, value)
+
 
 if __name__ == '__main__':
     x = read_content_from_file("../whatever.txt")
-    words = getSentencesOrWords(x, False)
-    word_counterr = number_of_occurrences_of_each_word(words)
-    print("Let me see how many occurrences of each word we have in your text: ")
-    for key, value in word_counterr.items():
-        print(key, value)
-    sentences = getSentencesOrWords(x, True)
-    print("\nYour longest sentence is:\n",max(sentences, key=len))
+    get_number_of_occurrences_of_each_word(x)
+    get_longest_sentence(x)
